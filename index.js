c7ba67e3b5c54fdef67892975f92d39134b7a4b1
Bodyuser <104937558+Bodyuser@users.noreply.github.com>
2023-10-24 22:24:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import jsonServer from 'json-server'
-import path from 'path'
+const jsonServer = require('json-server')
+const path = require('path')
 
 const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
